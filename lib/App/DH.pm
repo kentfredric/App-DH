0eb1a7910f0e6a5daaee273028710bf538781b92
Kent Fredric <kentfredric@gmail.com>
2013-02-14 09:56:19 +1300

    Build results of 71ca0f6 (on master)

--- lib/App/DH.pm
+++ lib/App/DH.pm
@@ -153,7 +153,7 @@ around print_usage_text => sub {
     } {
         $1 . ' ' . $list_cmds_opt
     }msex;
-  $text .= qq{\n} . $text . $list_cmds_usage qq{\n};
+  $text .= qq{\n} . $text . $list_cmds_usage . qq{\n};
   print $text or die "Cannot write to STDOUT";
   exit 0;
 };
