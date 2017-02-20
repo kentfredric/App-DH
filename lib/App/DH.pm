71e64bf58927dfc699c1c79fe6d024fa80b18593
Kent Fredric <kentfredric@gmail.com>
2017-02-20 21:42:34 +1300

    Prepare for a 0.4.900 trial series

--- lib/App/DH.pm
+++ lib/App/DH.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package App::DH;
 
-our $VERSION = '0.004001';
+our $VERSION = '0.004900';
 
 # ABSTRACT: Deploy your DBIx::Class Schema to DDL/Database via DBIx::Class::DeploymentHandler
 
